062b24ecadabfd5c7cc9be6894fe2103af04f2b8
Bianca Medeiros <102967064+Bianca0909@users.noreply.github.com>
2022-06-08 09:28:26 -0300

    new commit

---
+++ FarmaceuticoModel.java
@@ -0,0 +1,11 @@
+package br.com.triersistemas.andromeda.model;
+import lombok.Getter;
+import java.time.LocalDate;
+
+@Getter
+public class FarmaceuticoModel {
+  private String nome;
+  private LocalDate niver;
+  private String cpf;
+  private UUID uuid;
+}
