16e92e88654f3f6316757011df3d57dec1dcd230
Bianca Medeiros <biancamedeiros0909@gmail.com>
2022-06-09 17:21:50 -0300

    commit again

--- src/main/java/br/com/triersistemas/andromeda/domain/Pedido.java
+++ src/main/java/br/com/triersistemas/andromeda/domain/Pedido.java
@@ -3,6 +3,7 @@ package br.com.triersistemas.andromeda.domain;
 import org.apache.tomcat.jni.Local;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -19,9 +20,16 @@ public class Pedido {
     private BigDecimal valorPago;
     private BigDecimal troco;
 
+    public Pedido(final Cliente idClente, final LocalDate id, final String cpf, final String email) {
+
+
+    }
+
     public Pedido() {
 
     }
 
-
+    public Produto Pedido(final String nome, final LocalDate niver, final String cpf, final String email) {
+return null;
+    }
 }
